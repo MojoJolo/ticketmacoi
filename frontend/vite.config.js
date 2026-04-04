3c3c2de20c58ff9559d49438a770c4ebb298530f
Jolo Balbin <jolo.balbin@codapayments.com>
2026-04-04 20:46:01 +0800

    Blocked request. This host ("ticket-macoi.aitonomee.com") is not allowed. To allow this host, add "ticket-macoi.aitonomee.com" to `server.allowedHosts` in vite.config.js.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
+    allowedHosts: ['ticket-macoi.aitonomee.com'],
     proxy: {
       '/api': {
         target: 'http://backend:4000',
